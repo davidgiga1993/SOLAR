ec753d5e5efe54b22ead22b6c381d11f9b0c4563
Olli1511 <oliver.hamboerger@gmail.com>
2014-05-08 12:00:44 +0200

    end of Sprint

--- Project/Solar/src/com/me/stages/GameHUDStage.java
+++ Project/Solar/src/com/me/stages/GameHUDStage.java
@@ -2,11 +2,16 @@ package com.me.stages;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.me.solar.SolarEngine;
 
 public class GameHUDStage extends HUDStage{
