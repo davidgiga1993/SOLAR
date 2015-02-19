e48352fd63a1e7cc1cf617893e225cf24874d365
Th3Tw0 <thetwo@gmx.net>
2015-02-19 17:21:33 +0100

    OrbitalArrive done

--- Project/core/src/main/java/dhbw/karlsruhe/it/solar/core/ai/movement/OrbitalArriveSteeringProvider.java
+++ Project/core/src/main/java/dhbw/karlsruhe/it/solar/core/ai/movement/OrbitalArriveSteeringProvider.java
@@ -17,32 +17,32 @@ public class OrbitalArriveSteeringProvider extends ArriveSteeringProvider {
 
     @Override
     public Steering getSteering(Kinematic character) {
+        // Estimate traveling time:
         // Average Distance is the distance to the center of orbit.
         // Max-Distance = distanceToCenter + radius
         // Min-Distance = distanceToCenter - radius
+        // so the time to travel there at max speed gives us a very rough estimation
         Vector2 centerOfOrbit = target.getCenterOfOrbit();
         Vector2 directionToCenter = new Vector2(centerOfOrbit).sub(character.position);
-
-        float radius = target.getOrbitalRadiusInPixels();
-        float timeMargin = radius / character.maxSpeed;
-
         float distanceToCenter = directionToCenter.len();
         float timeToTravel = distanceToCenter / character.maxSpeed;
+
+        // Calculate target's position at that moment
         Vector2 futurePosition = target.calculateFuturePosition(timeToTravel);
 
+        // Calculate the distance between the character and the target's future position
         Vector2 directionToFuturePosition = new Vector2(futurePosition).sub(character.position);
         float newDistance = directionToFuturePosition.len();
 
-        if(newDistance < distanceToCenter) {
-            // Target will be closer to the character
-            futurePosition = target.calculateFuturePosition(timeToTravel - timeMargin);
-        }
-        if (newDistance > distanceToCenter) {
-            futurePosition = target.calculateFuturePosition(timeToTravel + timeMargin);
-        }
+        // Calculate the time error (e.g. waiting or missing) in this estimation
+        float timeError = newDistance - distanceToCenter;
+        timeError /= character.maxSpeed;
 
+        // Add the time error to our initial estimation and calculate the corrected future position
+        futurePosition = target.calculateFuturePosition(timeError + timeToTravel);
+
+        // Arrive at this position
         super.target = new Kinematic(futurePosition, 0,0,0);
-
         return super.getSteering(character);
     }
 
