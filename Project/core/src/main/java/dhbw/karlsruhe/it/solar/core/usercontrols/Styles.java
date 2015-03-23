92bde3b4dab1db1dc7969afb9e3555dd1b6f1802
Th3Tw0 <thetwo@gmx.net>
2015-03-23 21:01:29 +0100

    removed unused constructor

--- Project/core/src/main/java/dhbw/karlsruhe/it/solar/core/usercontrols/Styles.java
+++ Project/core/src/main/java/dhbw/karlsruhe/it/solar/core/usercontrols/Styles.java
@@ -3,13 +3,10 @@ package dhbw.karlsruhe.it.solar.core.usercontrols;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import dhbw.karlsruhe.it.solar.core.solar.FontCacher;
-import dhbw.karlsruhe.it.solar.core.solar.TextureCacher;
 
 public class Styles  {
     private static final BitmapFont DEFAULT_FONT = new BitmapFont();
@@ -19,11 +16,6 @@ public class Styles  {
 
     public static final Skin TOOLTIPSKIN = new Skin(Gdx.files.internal("data/skin/uiskin.json"));
 
-    public Styles(TextureCacher loader)    {
-        BitmapFont fontDefault = new BitmapFont();
-        FontCacher.addFont(fontDefault, "default");
-        TextureAtlas guiAtlas = new TextureAtlas(Gdx.files.internal("data/skin/uiskin.atlas"));
-        TOOLTIPSKIN.addRegions(guiAtlas);
-        // textButtonStyle = new TextButtonStyle(new NinePatchDrawable(loader.NinePatchMap.get("button_regular")), new NinePatchDrawable(loader.NinePatchMap.get("button_selected")), new NinePatchDrawable(loader.NinePatchMap.get("button_active")), new BitmapFont());
+    private Styles() {
     }
 }
